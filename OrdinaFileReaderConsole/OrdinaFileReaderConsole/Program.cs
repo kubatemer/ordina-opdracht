7bf64a593908fd8ebfce42272cb92fc55c0a3b02
Kubat <71670720+kubatemer@users.noreply.github.com>
2024-01-24 18:09:07 +0100

    version 6

--- OrdinaFileReaderConsole/OrdinaFileReaderConsole/Program.cs
+++ OrdinaFileReaderConsole/OrdinaFileReaderConsole/Program.cs
@@ -9,47 +9,69 @@ namespace OrdinaFileReaderConsole
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Ordina file reader");
-            Console.Write("\nPlease enter the path of the file or drag and drop the file here:");
-
-            try
+            while (true)
             {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("\n------------------------------------------------------------------");
+                Console.Write("\nEnter file path or drag and drop the file ('exit' to leave):");
                 string path = Console.ReadLine().Replace("\"", "");
-                Console.Clear();
 
-                if (!string.IsNullOrEmpty(path))
+                if (path == "exit")
                 {
-                    string extension = Path.GetExtension(path);
+                    Environment.Exit(0);
+                }
 
-                    if (extension.Equals(".txt", StringComparison.OrdinalIgnoreCase) || extension.Equals(".xml", StringComparison.OrdinalIgnoreCase))
+                try
+                {
+                    Console.Clear();
+
+                    if (!string.IsNullOrEmpty(path))
                     {
-                        string fileContent = File.ReadAllText(path);
+                        string extension = Path.GetExtension(path);
 
-                        if (extension.Equals(".txt", StringComparison.OrdinalIgnoreCase))
+                        if (extension.Equals(".txt", StringComparison.OrdinalIgnoreCase) || extension.Equals(".xml", StringComparison.OrdinalIgnoreCase))
                         {
-                            DisplayContent(fileContent);
-                            AskForDecryption(fileContent);
+                            string fileContent = File.ReadAllText(path);
+
+                            HandleFile(path, fileContent);
                         }
-                        else if (extension.Equals(".xml", StringComparison.OrdinalIgnoreCase))
+                        else
                         {
-                            HandleXmlFile(path, fileContent);
+                            DisplayError($"Given file (\"{path}\") is not a text or XML file.");
                         }
                     }
                     else
                     {
-                        DisplayError($"Given file (\"{path}\") is not a text or XML file.");
+                        DisplayError($"Given path (\"{path}\") is invalid or empty.");
                     }
                 }
-                else
+                catch (Exception)
                 {
                     DisplayError($"Given path (\"{path}\") is invalid or empty.");
+                    DisplayError("Something went wrong.");
                 }
             }
-            catch (Exception)
+        }
+
+        static void HandleFile(string path, string content)
+        {
+            Console.Write("Enter your role (admin/user): ");
+            string role = Console.ReadLine();
+
+            RoleSecurity roleBasedSecurity = new RoleSecurity();
+
+            if (roleBasedSecurity.CanReadFile(path, role))
             {
-                DisplayError("Something went wrong.");
+                Console.Clear();
+                DisplayContent(content);
+                AskForDecryption(content);
             }
-            Console.ReadKey();
+            else
+            {
+                DisplayError($"You do not have permission to read this file as {role}.");
+            }
         }
 
         static void DisplayContent(string content)
@@ -77,25 +99,6 @@ namespace OrdinaFileReaderConsole
             encryptionService.SetEncryption(new ReverseEncryption());
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Decrypted Content:\n\n" + encryptionService.DecryptText(content));
-        }
-
-        static void HandleXmlFile(string path, string content)
-        {
-            Console.Write("Enter your role (admin/user): ");
-            string role = Console.ReadLine();
-
-            RoleSecurity roleSecurity = new RoleSecurity();
-
-            if (roleSecurity.CanReadFile(path, role))
-            {
-                Console.Clear();
-                DisplayContent(content);
-                AskForDecryption(content);
-            }
-            else
-            {
-                DisplayError($"You do not have permission to read this file as {role}.");
-            }
         }
 
         static void DisplayError(string message)
